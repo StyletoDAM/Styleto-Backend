2b50432f06ed9028ab496294c4e893342ad39fcc
amine charrada <129897705+charrada-amine@users.noreply.github.com>
2025-11-28 16:04:23 +0100

    Update clothes.module.ts

--- src/clothes/clothes.module.ts
+++ src/clothes/clothes.module.ts
@@ -2,25 +2,27 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClothesService } from './clothes.service';
 import { ClothController } from './clothes.controller';
+import { DetectController } from './detect.controller'; // ✨ AJOUTER CETTE LIGNE
 import { Clothes, ClothesSchema } from './schemas/clothes.schema';
 import { User, UserSchema } from '../user/schemas/user.schema';
 import { UserPreferencesService } from './services/user-preferences.service';
 import { UserPreferences, UserPreferencesSchema } from './schemas/user-preferences.schema';
-import { SubscriptionsModule } from '../subscriptions/subscriptions.module'; // ✨ NOUVEAU
+import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
+
 @Module({
-imports: [
-MongooseModule.forFeature([
-{ name: Clothes.name, schema: ClothesSchema },
-{ name: User.name, schema: UserSchema },
-{ name: UserPreferences.name, schema: UserPreferencesSchema },
-]),
-SubscriptionsModule, // ✨ NOUVEAU
-],
-controllers: [ClothController],
-providers: [ClothesService, UserPreferencesService],
-exports: [
-ClothesService,
-MongooseModule.forFeature([{ name: Clothes.name, schema: ClothesSchema }]), // ✨ AJOUTÉ : Exporte le modèle Clothes pour l'utiliser dans d'autres modules comme EventsModule
-],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Clothes.name, schema: ClothesSchema },
+      { name: User.name, schema: UserSchema },
+      { name: UserPreferences.name, schema: UserPreferencesSchema },
+    ]),
+    SubscriptionsModule,
+  ],
+  controllers: [ClothController, DetectController], // ✨ AJOUTER DetectController ICI
+  providers: [ClothesService, UserPreferencesService],
+  exports: [
+    ClothesService,
+    MongooseModule.forFeature([{ name: Clothes.name, schema: ClothesSchema }]),
+  ],
 })
 export class ClothesModule {}
